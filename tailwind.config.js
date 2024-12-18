37289a394d9bf6e28b2ebd979072ffb7c0b12262
Thinakaran <thinakaranmanohar@yahoo.com>
2024-12-18 18:11:12 +0530

    hello

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
-module.exports = {
-content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// tailwind.config.js
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         upper: "upper",
         sftext: "sftext",
         para: "para",
-      }
+      },
     },
   },
   plugins: [],
-}
+};
